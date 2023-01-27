2e59e54aecac67c33897f5e5c89e2a1bad6883f5
jnip2 <jnip2@my.bcit.ca>
2023-01-27 13:06:22 -0800

    about page main area tests passing

--- pages/about.js
+++ pages/about.js
@@ -10,6 +10,68 @@ export default function About() {
                 <meta property="og:description" content="BCIT Digital Design and Development Diploma" />
                 <link rel="icon" href="/favicon.png" />
             </Head>
+            <main>
+                <div>
+                    <h1>
+
+                    </h1>
+                </div>
+                <div>
+                    <h1>
+
+                    </h1>
+                </div>
+                <div>
+                    <p>
+
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div>
+                    <h2>
+
+                    </h2>
+                </div>
+                <div>
+                    <h2>
+                        
+                    </h2>
+                </div>
+                <div>
+                    <h3>
+                        
+                    </h3>
+                </div>
+            </main>
         </>
     )
 }
