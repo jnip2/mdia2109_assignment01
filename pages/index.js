479bb9aa1562304dbbe3aad91f3ed5f00f3cbb71
jnip2 <jnip2@my.bcit.ca>
2023-02-08 19:56:26 -0800

    change html structure for styling layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Contact from './contact'
-import About from './about'
 import Link from 'next/link'
+import Navigation from "../Components/Navigation"
+
 
 export default function Home() {
   return (
@@ -18,31 +17,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.text__container}>
-          <h1 className={`${styles.h1}`}>An investment in knowledge pays the best interest.</h1>
-          <hr className={styles.hr} />
-          <p>
-            Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.
-          </p>
-          <Link href='/about'>
-            <button className={styles.button}>
-              More About Us
-            </button>
-          </Link>
-          <Link href='/contact'>
-            <button className={styles.button}>
-              Contact Us
-            </button>
-          </Link>
+        <Navigation />
+        <div className={styles.content__container}>
+          <div className={styles.text__container}>
+            <h1 className={`${styles.h1}`}>An investment in knowledge pays the best interest.</h1>
+            <hr className={styles.hr} />
+            <p>
+              Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.
+            </p>
+            <Link href='/about'>
+              <button className={styles.button}>
+                More About Us
+              </button>
+            </Link>
+            <Link href='/contact'>
+              <button className={styles.button}>
+                Contact Us
+              </button>
+            </Link>
+          </div>
+          <div className={styles.arrow__container}>
+            <Link href='/'>
+              <span>
+                <Image height={40} width={30} src="/icons/downwardArrow.png" />
+              </span>
+            </Link>
+          </div>
         </div>
-        <Link href='/'>
-          <span>
-            <img></img>
-          </span>
-        </Link>
-
       </main>
-
     </>
   )
 }
